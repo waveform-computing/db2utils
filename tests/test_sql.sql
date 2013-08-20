b67673b0695640c29f5b7af1773b147ac3e44ffa
Dave Hughes <dave@waveform.org.uk>
2013-08-20 17:42:20 +0000

    First stab at implementing #4
    Removes the awk-based testing query, adds the assertion framework (currently
    a mish-mash of procs and funcs) and migrates the existing tests to use the
    new assertion functions/procs

--- tests/test_sql.sql
+++ tests/test_sql.sql
@@ -1,8 +1,8 @@
-CALL ASSERT_EQUALS(QUOTE_STRING('A string'), '''A string''');
-CALL ASSERT_EQUALS(QUOTE_STRING('Frank''s string'), '''Frank''''s string''');
-CALL ASSERT_EQUALS(QUOTE_STRING('A multi' || X'0A' || 'line string'), '''A multi'' || X''0A'' || ''line string''');
-CALL ASSERT_EQUALS(QUOTE_IDENTIFIER('MY_TABLE'), 'MY_TABLE');
-CALL ASSERT_EQUALS(QUOTE_IDENTIFIER('MY#TABLE'), 'MY#TABLE');
-CALL ASSERT_EQUALS(QUOTE_IDENTIFIER('MyTable'), '"MyTable"');
-CALL ASSERT_EQUALS(QUOTE_IDENTIFIER('My "Table"'), '"My ""Table"""');
+VALUES ASSERT_EQUALS(QUOTE_STRING('A string'), '''A string''');
+VALUES ASSERT_EQUALS(QUOTE_STRING('Frank''s string'), '''Frank''''s string''');
+VALUES ASSERT_EQUALS(QUOTE_STRING('A multi' || X'0A' || 'line string'), '''A multi'' || X''0A'' || ''line string''');
+VALUES ASSERT_EQUALS(QUOTE_IDENTIFIER('MY_TABLE'), 'MY_TABLE');
+VALUES ASSERT_EQUALS(QUOTE_IDENTIFIER('MY#TABLE'), 'MY#TABLE');
+VALUES ASSERT_EQUALS(QUOTE_IDENTIFIER('MyTable'), '"MyTable"');
+VALUES ASSERT_EQUALS(QUOTE_IDENTIFIER('My "Table"'), '"My ""Table"""');
 
